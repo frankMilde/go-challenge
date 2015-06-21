d882ce5c0dffa1b40ae1e603e752a65d220b268a
Frank Milde <Frank.Milde@posteo.de>
2015-06-21 23:04:09 +0200

    Remove commented out code of stack list in grid.go

--- 04_repacking/grid.go
+++ 04_repacking/grid.go
@@ -120,26 +120,6 @@ func (g FreeGrid) String() string {
 	return s
 }
 
-// =========================================================================
-//  Implementing Sort interface
-//  Will order boxes from lowest to highest size.
-//  Use as:
-//          boxes = []box
-//          sort.Sort(BySize(boxes))
-//
-//	  			box{0, 0, 4, 4, 101},       box{0, 0, 2, 1, 103},
-//	  			box{0, 0, 2, 2, 102},  -->  box{0, 0, 2, 2, 102},
-//	  			box{0, 0, 2, 1, 103},       box{0, 0, 3, 2, 104},
-//	  			box{0, 0, 3, 2, 104},       box{0, 0, 4, 4, 101},
-// =========================================================================
-type ByArea []GridElement
-
-func (a ByArea) Len() int           { return len(a) }
-func (a ByArea) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByArea) Less(i, j int) bool { return a[i].s < a[j].s }
-
-// -----  end of Sort Interface  -----
-
 // Put takes a box b and puts it in the lower left corner of Gridelement e.
 // If b does not cover e completely, the remaining free space of grid e is
 // returned. This return value is of type FreeGrid := []GridElement and
@@ -228,158 +208,22 @@ func FreeGridsAreEqual(a, b FreeGrid) bool {
 	return true
 } // -----  end of function FreeGridssAreEqual  -----
 
-//func (s Stack) IsEmpty() bool { return len(s) == 0 }
+// =========================================================================
+//  Implementing Sort interface
+//  Will order boxes from lowest to highest size.
+//  Use as:
+//          boxes = []box
+//          sort.Sort(BySize(boxes))
 //
-//// Front returns last element of slice, which is the front of the stack
-//func (s Stack) Front() box {
-//	if s.IsEmpty() {
-//		return emptybox
-//	}
-//	return s[len(s)-1]
-//}
-//
-//// Push adds box b to stack pointer sp
-//func (sp *Stack) Push(b box) {
-//	*sp = append(*sp, b)
-//}
-//
-//func (sp *Stack) Pop() box {
-//	if (*sp).IsEmpty() {
-//		return emptybox
-//	}
-//
-//	last := len(*sp) - 1
-//	b := (*sp)[last]
-//	//	s[last] = nil // or the zero value of T
-//	(*sp) = (*sp)[:last]
-//	return b
-//}
-//
-//// StacksAreEqual compares two stacks s1,s2 and returns true if s1 and
-//// s2 have the same length and the same boxes at the same positions.
-//func StacksAreEqual(s1, s2 Stack) bool {
-//	if len(s1) != len(s2) {
-//		return false
-//	}
-//
-//	for i, b := range s1 {
-//		if !BoxesAreEqual(b, s2[i]) {
-//			return false
-//		}
-//	}
-//	return true
-//}
-//
-//// === old ===
-//
-///*
-//// Element is an element of a linked list.
-//type Element struct {
-//	next  *Element
-//	stack *Stack
-//	b     *box
-//}
-//
-//// Next returns the next list element or nil.
-//func (e *Element) Next() *Element {
-//	if p := e.next; e.stack != nil && p != &e.stack.root {
-//		return p
-//	}
-//	return nil
-//}
-//
-//// Box returns the box of element e.
-//func (e *Element) Box() *box {
-//	if e.b != nil {
-//		return e.b
-//	}
-//	return nil
-//}
-//
-//// Stack represents a singly-linked list.
-//// The zero value for Stack is an empty stack ready to use.
-//type Stack struct {
-//	root   Element
-//	length uint
-//}
-//
-//// Init initializes or clears stack s.
-//func (s *Stack) Init() *Stack {
-//	s.root.next = &s.root
-//	s.length = 0
-//	return s
-//}
-//
-//// New returns an initialized list.
-//func NewStack() *Stack {
-//	return new(Stack).Init()
-//}
-//
-//// Len returns the number of elements of stack s.
-//// The complexity is O(1).
-//func (s *Stack) Len() uint {
-//	return s.length
-//}
-//
-//// Front returns the first element of stack s or nil.
-//func (s *Stack) Front() *Element {
-//	if s.length == 0 {
-//		return nil
-//	}
-//	return s.root.next
-//}
-//
-//// Push inserts Element e created from box b at front of stack s, increments
-//// s.length.
-//func (s *Stack) Push(b *box) {
-//	r := &s.root
-//	n := r.next
-//
-//	e := &Element{n, s, b}
-//
-//	r.next = e
-//	s.length++
-//}
-//
-//// Pop removes first element e of stack s and decrements s.length.
-//// Pop returns nil if stack is empty or the *box from said first element.
-//func (s *Stack) Pop() *box {
-//
-//	if s.Len() == 0 {
-//		return nil
-//	}
-//
-//	f := s.Front()
-//
-//	s.root = *f.next
-//
-//	// avoid memory leaks
-//	f.next = nil
-//	f.stack = nil
-//
-//	s.length--
-//
-//	return f.b
-//}
-//
-//// StacksAreEqual compares to two stacks a,b and returns a true if a and b
-//// have the same length and the same boxes at the same positions in the
-//// stack.
-//func StacksAreEqual(a, b *Stack) bool {
-//	if a.Len() != b.Len() {
-//		return false
-//	}
-//
-//	if a.Len() == 0 && b.Len() == 0 {
-//		return true
-//	}
-//
-//	for p, q := &a.root, &b.root; p != nil && q != nil; p, q = p.Next(), q.Next() {
-//		if !BoxesAreEqual(*p.b, *q.b) {
-//			return false
-//		}
-//	}
-//
-//	return true
-//}
-//*/
+//	  			box{0, 0, 4, 4, 101},       box{0, 0, 2, 1, 103},
+//	  			box{0, 0, 2, 2, 102},  -->  box{0, 0, 2, 2, 102},
+//	  			box{0, 0, 2, 1, 103},       box{0, 0, 3, 2, 104},
+//	  			box{0, 0, 3, 2, 104},       box{0, 0, 4, 4, 101},
+// =========================================================================
+type ByArea []GridElement
+
+func (a ByArea) Len() int           { return len(a) }
+func (a ByArea) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByArea) Less(i, j int) bool { return a[i].s < a[j].s }
+
+// -----  end of Sort Interface  -----
